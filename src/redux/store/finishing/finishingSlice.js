f3abdfb8c6f76218a23feb9ad0fa7ccff25c0b2d
JoaquinWald <joaquingrunwald@gmail.com>
2023-08-27 16:48:49 -0300

    cancelled finishingSlice

--- src/redux/store/finishing/finishingSlice.js
+++ src/redux/store/finishing/finishingSlice.js
@@ -1,18 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+// import { createSlice } from '@reduxjs/toolkit';
 
-const finishingState = {};
+// const finishingState = {};
 
-export const finishingSlice = createSlice({
-	name: 'finishing',
+// export const finishingSlice = createSlice({
+// 	name: 'finishing',
 
-	initialState: finishingState,
+// 	initialState: finishingState,
 
-	reducers: {
-		increment: (state /* action */) => {
-			state.counter += 1;
-		},
-	},
-});
+// 	reducers: {
+// 		increment: (state /* action */) => {
+// 			state.counter += 1;
+// 		},
+// 	},
+// });
 
-// Action creators are generated for each case reducer function
-export const { increment } = finishingSlice.actions;
+// // Action creators are generated for each case reducer function
+// export const { increment } = finishingSlice.actions;
